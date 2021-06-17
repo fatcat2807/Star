19d679fde0d9a2f47455cc79ad52b2cba517c612
fatcat2807 <55304978+fatcat2807@users.noreply.github.com>
2021-06-17 07:01:39 +0700

    Update NexusService.sql

--- NexusService.sql
+++ NexusService.sql
@@ -20,10 +20,10 @@ create table Product
 	ConnectionType varchar(50),
 	Price money,
 	PriceType varchar(50),
+	[image] varchar(250) not null,
 	UsedTime datetime,
 	Assess varchar(250),
 	[status] bit,
-	roll bit
 )
 
 create table Customer
@@ -34,6 +34,7 @@ create table Customer
 	Email varchar(50),
 	Phone varchar(10),
 	[status] bit,
+	[Role] varchar(5),
 )
 create table Bill
 (
@@ -61,7 +62,6 @@ create table Staff
 	[Name] varchar(50),
 	Pass varchar(50),
 	Age int,
-	Sex varchar(50),
 	Id_Store int,
 	[status] bit,
 )
@@ -75,7 +75,7 @@ create table ProductDetails
 	Information varchar(250),
 )
 
-create table Plane
+create table Plans
 (
 	Id_Plane int identity primary key,
 	Id_Customer varchar(50),
@@ -90,6 +90,13 @@ create table Plane
 	Price money,
 )
 
+create table Messenger
+(
+	Id_Customer varchar(50),
+	[Name] varchar(50),
+	content varchar(250),
+	
+)
 
 ALTER TABLE Bill
 Add FOREIGN KEY (Id_Product) REFERENCES Product(Id_Product);
@@ -109,8 +116,23 @@ ADD FOREIGN KEY (Id_Store) REFERENCES Store(Id_Store);
 ALTER TABLE EquipmentManagement
 Add FOREIGN KEY (Id_Customer) REFERENCES Customer(Id_Customer);
 
-ALTER TABLE Plane
+ALTER TABLE Plans
 Add FOREIGN KEY (Id_Bill) REFERENCES Bill(Id_Bill);
 
-ALTER TABLE Plane
+ALTER TABLE Plans
 Add FOREIGN KEY (Id_Store) REFERENCES Store(Id_Store);
+
+ALTER TABLE Messenger
+Add FOREIGN KEY (Id_Customer) REFERENCES Customer(Id_Customer);
+
+insert into Customer (Id_Customer, [Name], Pass, Email, Phone, [status], [Role]) 
+values ('1' ,'admin', '1234', 'admin@gmail.com', '0168461864' , 1, 'admin');
+
+insert into Customer (Id_Customer, [Name], Pass, Email, Phone, [status], [Role]) 
+values ('D000D00000000001' ,'user', '123', 'user@gmail.com', '4616486138' , 1, 'user');
+
+insert into Product (Id_Product, Name_Product,  ConnectionType, Price, [image], UsedTime, Assess, [status]) 
+values (1 ,'ST10K', '1 ngay', 10, 'ST10K.png' , 10/10/2021 , '10k', 1);
+
+insert into Store (Id_Store, Name_Store, Id_Product, ConnectionType, [status]) 
+values (1 ,'Viettle', 1, '1 ngay', 1);
